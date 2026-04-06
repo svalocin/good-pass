d68341769264b1cee324dafadf789e8b10a42f47
Svalocin <shiwei1211@gmail.com>
2026-04-06 23:07:38 +0800

    refactor: 重构 v3.0.0

--- sw.js
+++ sw.js
@@ -1,81 +1,55 @@
+const CACHE_KEY = 'good-pass_v3.0.0';
 
-console.log('Script loaded!')
-var cacheStorageKey = '9f2c3b61-0f1f-4a8a-b332-71a8ad2dc303_2.6.0'
-
-var cacheList = [
+const CACHE_LIST = [
   '/',
-  "index.html",
-  "css/mustard-ui.min.css",
-  "js/md5.min.js",
-  "js/jquery-3.4.1.min.js",
-  "img/android-icon-144x144.png",
-  "img/android-icon-192x192.png",
-  "img/android-icon-36x36.png",
-  "img/android-icon-48x48.png",
-  "img/android-icon-72x72.png",
-  "img/android-icon-96x96.png",
-  "img/apple-icon-114x114.png",
-  "img/apple-icon-120x120.png",
-  "img/apple-icon-144x144.png",
-  "img/apple-icon-152x152.png",
-  "img/apple-icon-180x180.png",
-  "img/apple-icon-57x57.png",
-  "img/apple-icon-60x60.png",
-  "img/apple-icon-72x72.png",
-  "img/apple-icon-76x76.png",
-  "img/apple-icon-precomposed.png",
-  "img/apple-icon.png",
-  "img/favicon-16x16.png",
-  "img/favicon-32x32.png",
-  "img/favicon-96x96.png",
-  "img/favicon.ico",
-  "img/ms-icon-144x144.png",
-  "img/ms-icon-150x150.png",
-  "img/ms-icon-310x310.png",
-  "img/ms-icon-70x70.png"
-]
+  'index.html',
+  'js/md5.min.js',
+  'manifest.json',
+  'img/android-icon-144x144.png',
+  'img/android-icon-192x192.png',
+  'img/android-icon-36x36.png',
+  'img/android-icon-48x48.png',
+  'img/android-icon-72x72.png',
+  'img/android-icon-96x96.png',
+  'img/apple-icon-114x114.png',
+  'img/apple-icon-120x120.png',
+  'img/apple-icon-144x144.png',
+  'img/apple-icon-152x152.png',
+  'img/apple-icon-180x180.png',
+  'img/apple-icon-57x57.png',
+  'img/apple-icon-60x60.png',
+  'img/apple-icon-72x72.png',
+  'img/apple-icon-76x76.png',
+  'img/apple-icon-precomposed.png',
+  'img/apple-icon.png',
+  'img/favicon-16x16.png',
+  'img/favicon-32x32.png',
+  'img/favicon-96x96.png',
+  'img/favicon.ico',
+  'img/ms-icon-144x144.png',
+  'img/ms-icon-150x150.png',
+  'img/ms-icon-310x310.png',
+  'img/ms-icon-70x70.png',
+];
 
-self.addEventListener('install', function (e) {
-  console.log('Cache event!')
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheStorageKey).then(function (cache) {
-      console.log('Adding to Cache:', cacheList)
-      return cache.addAll(cacheList)
-    }).then(function () {
-      console.log('Skip waiting!')
-      return self.skipWaiting()
-    })
-  )
-})
+    caches.open(CACHE_KEY)
+      .then(cache => cache.addAll(CACHE_LIST))
+      .then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener('activate', function (e) {
-  console.log('Activate event')
+self.addEventListener('activate', e => {
   e.waitUntil(
-    Promise.all(
-      caches.keys().then(cacheNames => {
-        return cacheNames.map(name => {
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-        })
-      })
-    ).then(() => {
-      console.log('Clients claims.')
-      return self.clients.claim()
-    })
-  )
-})
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_KEY).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
 
-self.addEventListener('fetch', function (e) {
-  // console.log('Fetch event:', e.request.url)
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
-      if (response != null) {
-        console.log('Using cache for:', e.request.url)
-        return response
-      }
-      console.log('Fallback to fetch:', e.request.url)
-      return fetch(e.request.url)
-    })
-  )
-}) 
+    caches.match(e.request).then(cached => cached ?? fetch(e.request))
+  );
+});
